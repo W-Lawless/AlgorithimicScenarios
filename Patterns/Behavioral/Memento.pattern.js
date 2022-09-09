9e90eb62e3c99e881dbb8872fdd760622b0304ed
W Lawless <wLawless.sharpe@protonmail.com>
2022-09-08 21:16:32 -0400

    Memento Pattern

--- Patterns/Behavioral/Memento.pattern.js
+++ Patterns/Behavioral/Memento.pattern.js
@@ -1,20 +1,47 @@
 
 
-//! Memento Pattern
+//! Memento (Snapshot) Pattern
 //! Example Case: Bank Account 
 
 class BankAccount {
     constructor(balance = 0) {
         this.balance = balance;
+        this.changes = [new Memento(balance)]
+        this.current = 0
     }
 
     deposit(amount) {
         this.balance += amount
-        return new Memento(this.balance)
+        const m = new Memento(this.balance)
+        this.changes.push(m)
+        this.current++
+        return m
     }
 
     rollbackBalance(memento){
-        this.balance = memento.balance
+        if(memento) {
+            this.balance = memento.balance 
+            this.changes.push(memento)
+            this.current = this.changes.count - 1
+        }
+    }
+
+    undo() { 
+        if (this.current > 0) {
+            let memento = this.changes[--this.current]
+            this.balance = memento.balance
+            return memento
+        }
+        return null
+    }
+
+    redo() {
+        if(this.current + 1 < this.changes.length) {
+            let memento = this.changes[++this.current]
+            this.balance = memento.balance
+            return memento
+        }
+        return null
     }
 
     toString() {
@@ -33,9 +60,21 @@ const memento1 = ba.deposit(20)
 const memento2 = ba.deposit(35)
 
 console.log(ba.toString())
+//* -> Balance: 155
 
-ba.rollbackBalance(memento1)
-console.log(ba.toString())
+// ba.rollbackBalance(memento1)
+// console.log(ba.toString())
+// //* -> Balance: 120
 
-ba.rollbackBalance(memento2)
-console.log(ba.toString())
+// ba.rollbackBalance(memento2)
+// console.log(ba.toString())
+// //* -> Balance: 155
+
+ba.undo()
+console.log('Undo 1',ba.toString())
+
+ba.undo()
+console.log('Undo 2',ba.toString())
+
+ba.redo()
+console.log('Redo 1',ba.toString())
